23067040757df948e25421a7d61224e887104ef1
Siddharth Diwan <70694457+sidwan02@users.noreply.github.com>
2022-11-20 15:04:16 -0500

    added logic for startup task creation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <atmel_start.h>
+#include "rtos_tasks_config.h"
 
 int main(void)
 {
@@ -11,8 +12,22 @@ int main(void)
 	// TODO:
 	/*
 	1. How do we create a thread process
-	2. How does the rtos global pq work
+	2. How does the FreeRTos global pq work
 	3. How do we create the waitq for the gatekeeper tasks
 	4. Do we need to define certain structs for different tasks (gatekeeper struct, sensor struct, etc)
 	*/
+
+	// create first init task to start RTOS and other tasks
+	xTaskCreateStatic(startup_task,
+					  "initializer task",
+					  TASK_INIT_STACK_SIZE,
+					  NULL,
+					  TASK_INIT_PRIORITY,
+					  init_task_stack,
+					  &init_task_buffer);
+}
+
+void startup_task(void *pvParameters)
+{
+	print("PVDXos starting.................................................");
 }
